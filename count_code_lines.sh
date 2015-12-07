c5cc2c05786e0ffbbccaac82f14d836d4073ecb3
Lilac <995967851@qq.com>
2015-12-07 19:04:18 +0800

    update count code line

--- count_code_lines.sh
+++ count_code_lines.sh
@@ -2,5 +2,4 @@
 
 # 使用方法： ./count_code_line.sh your_project_dir
 
-find -s $1 "(" -name "*.m" -or -name "*.mm" -or -name "*.cpp" -or -name "*.h" -or -name "*.rss" ")" -print | xargs wc -l
-
+find -s $1 "(" -name "*.m" -or -name "*.mm" -or -name "*.cpp" -or -name "*.h" -or -name "*.rss" ")" -print | xargs grep -v "^$"|wc -l
